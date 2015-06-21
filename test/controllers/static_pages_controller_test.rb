cf7c2d4406011dfa114d0494bb820f6a0547aaa5
kobestarr <kobiomenaka@yahoo.co.uk>
2015-06-21 16:47:30 +0100

    made title test pass

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,19 +4,19 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Kobestarr on Rails Tutorial Sample App"
+    assert_select "title", "Kobestarr on Rails Tutorial Sample App"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | Kobestarr on Rails Tutorial Sample App"
+    assert_select "title", "Kobestarr on Rails Tutorial Sample App"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | Kobestarr on Rails Tutorial Sample App"
+    assert_select "title", "Kobestarr on Rails Tutorial Sample App"
   end
 
 end
